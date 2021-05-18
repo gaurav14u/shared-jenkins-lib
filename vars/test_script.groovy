18587bc8559a8a60db8486cfaea7d355265913b9
gaurav14u <gaurav14u@gmail.com>
2021-05-18 20:58:13 +0200

    Update test_script.groovy

--- vars/test_script.groovy
+++ vars/test_script.groovy
@@ -1,5 +1,12 @@
 def build(){
   echo 'build app'
+  System.getProperty("user.dir");
+  def proc = "ls".execute()
+  def b = new StringBuffer()
+  proc.consumeProcessErrorStream(b)
+  println proc.text
+  println b.toString()
+
 }
 
 def test(){
